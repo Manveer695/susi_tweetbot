1d53da0f1b552a309836d2ee0baebd1d2fe5c0ee
Manveer695 <singhmanveer1995@gmail.com>
2017-08-11 10:27:12 +0530

    web results clickable

--- index.js
+++ index.js
@@ -302,17 +302,7 @@ function TwitterBot() {
 		                      },
 		                      "message_data": {
 		                        "text": txt,
-							  	"entities": {
-							    	"hashtags": [],
-							    	"symbols": [],
-							    	"urls": [{
-							      		"url": "https:\/\/t.co\/XdXRudPXH5",
-							      		"expanded_url": "https:\/\/blog.twitter.com\/2013\/rich-photo-experience-now-in-embedded-tweets-3",
-							      		"display_url": "blog.twitter.com\/2013\/rich-phot\u2026",
-							      		"indices": [80, 103]
-							    	}],
-							    	"user_mentions": []
-							  	}
+		                        "url": "https://www.github.com"
 		                      }
 		                    }
 		                  }
